91ce337be2a7207be81cee3573dbc0497db42ac5
unknown <oseiasc2@gmail.com>
2024-01-17 18:10:35 -0300

    fix: fixed responsiveness

--- app/components/about/about.styles.ts
+++ app/components/about/about.styles.ts
@@ -7,13 +7,24 @@ export const AboutContainer = styled.section`
     color: #fff;
     justify-content: center;
     align-items: center;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 80px;
+    padding-bottom: 80px;   
+
     @media(max-width: 800px){
         flex-wrap: wrap;
+        padding-top: 40px;
+        padding-bottom: 40px; 
     }
 `
 
 export const AboutImg = styled(Image)`
-
+    @media(max-width: 800px){
+        width: 100%;
+        height: auto;
+        margin-top: 40px;    
+    }
 `
 
 export const AbountContent = styled.div`
